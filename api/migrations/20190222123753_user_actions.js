30ea3d82b1a9074bdd1af304c7b5f19e9b986b78
Breno Mazieiro <breno.mazieiro@gmail.com>
2019-11-09 17:00:44 -0300

    BREAD - changing migrations to add type on users_actions

--- api/migrations/20190222123753_user_actions.js
+++ api/migrations/20190222123753_user_actions.js
@@ -1,9 +1,10 @@
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
     return knex.raw(`
     CREATE TABLE IF NOT EXISTS users_actions (
         id_user_action INT UNSIGNED NOT NULL AUTO_INCREMENT,
         name VARCHAR(64) NOT NULL,
         description VARCHAR(256) NULL,
+        type varchar(1) NOT NULL,
         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP(),
         updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP() ON UPDATE CURRENT_TIMESTAMP(),
         deleted_at DATETIME NULL DEFAULT NULL,
@@ -13,11 +14,11 @@ exports.up = function(knex, Promise) {
         deleted TINYINT(1) NOT NULL DEFAULT 0,
         PRIMARY KEY (id_user_action),
         INDEX deleted (deleted ASC) ,
-        UNIQUE INDEX name_UNIQUE (deleted ASC, name ASC) )
+        UNIQUE INDEX name_UNIQUE (deleted ASC, name ASC, type ASC) )
       ENGINE = InnoDB;
     `)
   }
   
-  exports.down = function(knex, Promise) {
-    return knex.schema.dropTable('users_actions');
+  exports.down = function(knex) {
+    return knex.schema.dropTable('users_actions')
   }
--- api/migrations/20191101130243_seed.js
+++ api/migrations/20191101130243_seed.js
@@ -5,33 +5,105 @@ exports.up = function(knex) {
     `),
     knex.raw(`
       INSERT INTO users_profiles(id_user_profile, name, slug, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES(2, 'Subscribers', 'subscriber', '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `), 
+
+    knex.raw(`
+      INSERT INTO users_actions(id_user_action, name, description, type, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES(1, 'Users', 'user managment', 'b','2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
     `),
     knex.raw(`
-      INSERT INTO users_actions(id_user_action, name, description, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES(1, 'UserLogin', 'user login', '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+      INSERT INTO users_actions(id_user_action, name, description, type, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES(2, 'Users', 'user managment', 'r','2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
     `),
     knex.raw(`
-      INSERT INTO users_actions(id_user_action, name, description, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES(2, 'Users', 'user managment', '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+      INSERT INTO users_actions(id_user_action, name, description, type, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES(3, 'Users', 'user managment', 'e','2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
     `),
     knex.raw(`
-      INSERT INTO users_actions(id_user_action, name, description, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES(3, 'Profiles', 'profile management', '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+      INSERT INTO users_actions(id_user_action, name, description, type, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES(4, 'Users', 'user managment', 'a','2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
     `),
     knex.raw(`
-      INSERT INTO users_actions(id_user_action, name, description, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES(4, 'Actions', 'action management', '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+      INSERT INTO users_actions(id_user_action, name, description, type, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES(5, 'Users', 'user managment', 'd','2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+
+
+    knex.raw(`
+      INSERT INTO users_actions(id_user_action, name, description, type, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES(6, 'Profiles', 'profile management', 'b','2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
     `),
     knex.raw(`
-      INSERT INTO profiles_actions_users(id_profile_action_user, id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (1, 1, 1, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+      INSERT INTO users_actions(id_user_action, name, description, type, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES(7, 'Profiles', 'profile management', 'r','2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
     `),
     knex.raw(`
-      INSERT INTO profiles_actions_users(id_profile_action_user, id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (2, 1, 2, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+      INSERT INTO users_actions(id_user_action, name, description, type, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES(8, 'Profiles', 'profile management', 'e','2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
     `),
     knex.raw(`
-      INSERT INTO profiles_actions_users(id_profile_action_user, id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (3, 1, 3, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+      INSERT INTO users_actions(id_user_action, name, description, type, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES(9, 'Profiles', 'profile management', 'a','2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
     `),
     knex.raw(`
-      INSERT INTO profiles_actions_users(id_profile_action_user, id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (4, 1, 4, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+      INSERT INTO users_actions(id_user_action, name, description, type, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES(10, 'Profiles', 'profile management', 'd','2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+
+    knex.raw(`
+      INSERT INTO users_actions(id_user_action, name, description, type, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES(11, 'Actions', 'action management', 'b','2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
     `),
     knex.raw(`
-      INSERT INTO profiles_actions_users(id_profile_action_user, id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (5, 2, 1, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+      INSERT INTO users_actions(id_user_action, name, description, type, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES(12, 'Actions', 'action management', 'r','2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+    knex.raw(`
+      INSERT INTO users_actions(id_user_action, name, description, type, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES(13, 'Actions', 'action management', 'e','2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+    knex.raw(`
+      INSERT INTO users_actions(id_user_action, name, description, type, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES(14, 'Actions', 'action management', 'a','2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+    knex.raw(`
+      INSERT INTO users_actions(id_user_action, name, description, type, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES(15, 'Actions', 'action management', 'd','2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+
+    knex.raw(`
+      INSERT INTO profiles_actions_users(id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (1, 1, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+    knex.raw(`
+      INSERT INTO profiles_actions_users(id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (1, 2, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+    knex.raw(`
+      INSERT INTO profiles_actions_users(id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (1, 3, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+    knex.raw(`
+      INSERT INTO profiles_actions_users(id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (1, 4, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+    knex.raw(`
+      INSERT INTO profiles_actions_users(id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (1, 5, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+    knex.raw(`
+      INSERT INTO profiles_actions_users(id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (1, 6, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+    knex.raw(`
+      INSERT INTO profiles_actions_users(id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (1, 7, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+    knex.raw(`
+      INSERT INTO profiles_actions_users(id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (1, 8, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+    knex.raw(`
+      INSERT INTO profiles_actions_users(id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (1, 9, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+    knex.raw(`
+      INSERT INTO profiles_actions_users(id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (1, 10, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+    knex.raw(`
+      INSERT INTO profiles_actions_users(id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (1, 11, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+    knex.raw(`
+      INSERT INTO profiles_actions_users(id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (1, 12, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+    knex.raw(`
+      INSERT INTO profiles_actions_users(id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (1, 13, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+    knex.raw(`
+      INSERT INTO profiles_actions_users(id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (1, 14, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+    knex.raw(`
+      INSERT INTO profiles_actions_users(id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (1, 15, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
+    `),
+    
+    knex.raw(`
+      INSERT INTO profiles_actions_users(id_user_profile, id_user_action, created_at, updated_at, deleted_at, created_by, updated_by, deleted_by, deleted) VALUES (2, 1, '2019-08-29 11:54:37.000', '2019-08-29 11:54:37.000', NULL, 1, 1, NULL, 0);
     `),
   ])
 }
