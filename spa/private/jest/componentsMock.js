4fcea08f5dda334ec3f397bf6c6e32067b8bc22f
Breno Mazieiro <breno.mazieiro@gmail.com>
2019-12-28 11:04:29 +0000

    Updating mock when function

--- spa/private/jest/componentsMock.js
+++ spa/private/jest/componentsMock.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types'
 module.exports = new Proxy({}, {
   get: (target, property) => {
     const Mock = (props) => {
+      if(!props) return () => false
       const { children } = props
       return <span>{children}</span>
     }
