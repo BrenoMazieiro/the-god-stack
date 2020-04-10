fec147258b5ac15c980d685ea49a03bd72fb9fc9
Breno Mazieiro <breno.mazieiro@gmail.com>
2020-04-10 14:31:45 +0100

    Structure for a new SPA

---
+++ newspa/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Block } from 'components'
+
+// import { Container } from './styles';
+
+const App = () => {
+  return (
+    <Block> Breno </Block>
+  );
+}
+
+export default App
---
+++ newspa/src/components/atoms/Block/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default ({ children }) => {
+  return (
+  <div> {children} </div>
+  );
+}
+
