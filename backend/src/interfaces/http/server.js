61a75443a394a970d87b1442c3cc3ab49e1a4ede
Breno Mazieiro <breno.mazieiro@gmail.com>
2021-03-20 22:32:02 +0000

    will change ngnix for traefik

--- backend/src/interfaces/http/server.js
+++ backend/src/interfaces/http/server.js
@@ -28,9 +28,9 @@ const server = new APS.ApolloServer({
 })
 
 server.init = () => {
-  server.listen(process.env.VIRTUAL_PORT || 3000, '0.0.0.0').then(() => {
+  server.listen(process.env.PORT || 3000, '0.0.0.0').then(() => {
     // eslint-disable-next-line no-console
-    console.log('\x1b[36m%s\x1b[0m', 'Server started successfully!')
+    console.log('\x1b[36m%s\x1b[0m', `Server started successfully on port: ${process.env.PORT || 3000} !`)
   })
 }
 
