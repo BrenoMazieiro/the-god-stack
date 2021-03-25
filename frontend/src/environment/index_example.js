699af82495553737c16041c806f4c2ab8868c8c1
Breno Mazieiro <breno.mazieiro@gmail.com>
2021-03-25 00:07:23 +0000

    will create environment on ci process

---
+++ frontend/src/environment/index_example.js
@@ -0,0 +1,4 @@
+export const HTTP_API_URL = 'http://api.local.thegodstack.com/'
+export const WS_API_URL = 'ws://api.local.thegodstack.com/graphql'
+export const CLIENT = 'thegodstack'
+export const DEBUG = 'true'
