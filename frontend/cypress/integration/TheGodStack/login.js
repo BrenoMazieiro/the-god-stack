bf31154a5f1a80e2deafaeb9260278491347f7f8
Breno Mazieiro <breno.mazieiro@gmail.com>
2021-05-23 14:54:03 +0100

    will improve login cypress

--- frontend/cypress/integration/TheGodStack/login.js
+++ frontend/cypress/integration/TheGodStack/login.js
@@ -2,24 +2,40 @@ import { HTTP_SPA_URL } from '../../../src/environment'
 
 context('Login', () => {
   beforeEach(() => {
-    cy.visit(HTTP_SPA_URL)
+    cy.visit(`${HTTP_SPA_URL}login`)
   })
   it('will login with valid user', () => {
-    cy.get('#username')
-      .type('breno.mazieiro').should('have.value', 'breno.mazieiro')
-    cy.get('#password')
-      .type('12345678').should('have.value', '12345678')
-    cy.get('[type="submit"]').click()
-    cy.get('#homepage').should('exist')
-    cy.get('[type="button"]').click()
+    cy
+      .get('input#username')
+      .type('breno.mazieiro')
+      .should('have.value', 'breno.mazieiro')
+    cy
+      .get('input#password')
+      .type('12345678')
+      .should('have.value', '12345678')
+    cy
+      .get('[type="submit"]')
+      .click()
+    cy
+      .get('#MainTemplate')
+      .should('exist')
+    cy
+      .get('[type="button"]')
+      .click()
   })
 
   it('will not login with invalid user', () => {
-    cy.get('#username')
-      .type('breno.mazieiro').should('have.value', 'breno.mazieiro')
-    cy.get('#password')
-      .type('1234567').should('have.value', '1234567')
-    cy.get('[type="submit"]').click()
-    cy.get('#errorMessage').should('contain.text', 'Invalid username or password!')
+    cy.get('input#username')
+      .type('breno.mazieiro')
+      .should('have.value', 'breno.mazieiro')
+    cy.get('input#password')
+      .type('1234567')
+      .should('have.value', '1234567')
+    cy
+      .get('[type="submit"]')
+      .click()
+    cy
+      .get('#errorMessage')
+      .should('contain.text', 'Invalid username or password')
   })
 })
