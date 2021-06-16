6912afe5bc1f0eca8c5c0fbac78bd87deb27dd61
Breno Mazieiro <breno.mazieiro@gmail.com>
2021-06-16 22:32:49 +0100

    will isolate ci on a folder

---
+++ ci/addHosts.sh
@@ -0,0 +1,4 @@
+echo '127.0.0.1   proxy.ci.thegodstack.com' >> /etc/hosts
+echo '127.0.0.1   pg.ci.thegodstack.com' >> /etc/hosts
+echo '127.0.0.1   api.ci.thegodstack.com' >> /etc/hosts
+echo '127.0.0.1   spa.ci.thegodstack.com' >> /etc/hosts
--- ci/ci.sh
+++ ci/ci.sh
@@ -26,9 +26,11 @@ SHOW_H1 () {
   printf "\n\n${MESSAGE}### $1${NC}\n"
 }
 
+cd ..
+
 if [ ! -f .env ]; then
   SHOW_H1 "Creating .env file"
-  if (cp .env_example_local .env) ; then
+  if (cp ./ci/env_example_ci .env) ; then
     SHOW_SUCCESS ".env file successfully created!"
   else
     SHOW_ERROR "I was not able to create the .env file, plese check the errors above!"
@@ -39,7 +41,7 @@ fi
 
 if [ ! -f docker-compose.yml ]; then
   SHOW_H1 "Creating docker-compose.yml file"
-  if (cp docker-compose-ci.yml docker-compose.yml) ; then
+  if (cp ./ci/docker-compose-ci.yml docker-compose.yml) ; then
     SHOW_SUCCESS "docker-compose.yml file successfully created!"
   else
     SHOW_ERROR "I was not able to create the docker-compose.yml file, plese check the errors above!"
