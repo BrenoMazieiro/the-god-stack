fa625847ee3311546f230fe96f054b529a5343da
Breno Mazieiro <breno.mazieiro@gmail.com>
2021-07-04 18:00:30 +0100

    will use new layout on SigInForm

--- frontend/src/components/organisms/Forms/SignInForm/index.jsx
+++ frontend/src/components/organisms/Forms/SignInForm/index.jsx
@@ -3,15 +3,15 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 import { useMyContext } from 'hooks'
 import {
-  Form, LabeledInput, Button, Div, A,
+  Form, Button, A, Textfield, Text,
 } from 'components'
 
-const ErrorWrapper = styled(Div)`
+const StyledText = styled(Text)`
   display: flex;
   align-items: center;
   align-self: center;
 `
-const LinkWrapper = styled(Div)`
+const LinkWrapper = styled(Text)`
   display: flex;
   align-items: center;
   align-self: center;
@@ -24,27 +24,33 @@ const SignInForm = ({
   const { t } = useMyContext()
   return (
     <Form handleSubmit={handleSubmit} id="SignInForm">
-      <LabeledInput
-        type="text"
+      <Textfield
         id="username"
-        label={t['organisms:Forms:SignInForm:Username']}
-        ref={username}
+        type="text"
+        reference={username}
         placeholder={t['organisms:Forms:SignInForm:Username']}
-        errorMessages={[]}
+        status={errorMessage ? 'error' : null}
       />
-      <LabeledInput
-        type="password"
+      <Textfield
         id="password"
-        label={t['organisms:Forms:SignInForm:Password']}
-        ref={password}
+        type="password"
+        reference={password}
         placeholder={t['organisms:Forms:SignInForm:Password']}
-        errorMessages={[]}
+        status={errorMessage ? 'error' : null}
       />
-      <Button type="submit" size="md" bgColor="blue" color="white">{t['organisms:Forms:SignInForm:SignIn']}</Button>
+      <Button
+        type="submit"
+        size="md"
+        bgColor="blue"
+        color="white"
+        fullWidth
+      >
+        {t['organisms:Forms:SignInForm:SignIn']}
+      </Button>
       {errorMessage && (
-        <ErrorWrapper id="errorMessage">
+        <StyledText color="red">
           {t[`organisms:Forms:SignInForm:${errorMessage}`]}
-        </ErrorWrapper>
+        </StyledText>
       )}
       <LinkWrapper>
         {t['organisms:Forms:SignInForm:notSignedUp']}
