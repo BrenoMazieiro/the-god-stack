113f2bfdb958e85fb45b216a9843586bbaef024d
Breno Mazieiro <breno.mazieiro@gmail.com>
2021-07-04 17:47:26 +0100

    will fix Text test name

--- frontend/src/components/atoms/Text/index.test.js
+++ frontend/src/components/atoms/Text/index.test.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { shallow } from 'enzyme'
-import Button from '.'
+import Text from '.'
 
-const wrap = ({ children, id }) => shallow(<Button id={id}>{children}</Button>)
+const wrap = ({ children, id }) => shallow(<Text id={id}>{children}</Text>)
 
-describe('Button', () => {
-  it('will render a large Button component when no size is sent', () => {
+describe('Text', () => {
+  it('will render a large Text component when no size is sent', () => {
     const wrapper = wrap({ children: 'Teste', id: 'mytext' })
     expect(wrapper.find({ id: 'mytext' })).toHaveLength(1)
   })
