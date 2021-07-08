da5411fb08075a34df676538a94ac62d1aa47c0e
Breno Mazieiro <breno.mazieiro@gmail.com>
2021-07-08 23:59:44 +0100

    will add more icons to icon component

--- frontend/src/components/atoms/Icon/index.jsx
+++ frontend/src/components/atoms/Icon/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { FaPowerOff, FaAnchor, FaArrowAltCircleRight } from 'react-icons/fa'
+import {
+  FaPowerOff, FaAnchor, FaArrowAltCircleRight, FaRegUserCircle, FaRegEnvelope, FaRegIdCard, FaLock, FaExclamationTriangle,
+} from 'react-icons/fa'
 import styled, { css } from 'styled-components'
 
 const genericDesing = css`
@@ -12,6 +14,11 @@ const genericDesing = css`
 const StyledFaPowerOff = styled(FaPowerOff)`${genericDesing}`
 const StyledFaAnchor = styled(FaAnchor)`${genericDesing}`
 const StyledFaArrowAltCircleRight = styled(FaArrowAltCircleRight)`${genericDesing}`
+const StyledFaRegUserCircle = styled(FaRegUserCircle)`${genericDesing}`
+const StyledFaRegEnvelope = styled(FaRegEnvelope)`${genericDesing}`
+const StyledFaRegIdCard = styled(FaRegIdCard)`${genericDesing}`
+const StyledFaLock = styled(FaLock)`${genericDesing}`
+const StyledFaExclamationTriangle = styled(FaExclamationTriangle)`${genericDesing}`
 
 const Icon = ({
   iconname, iconsize, className,
@@ -21,13 +28,18 @@ const Icon = ({
       power: <StyledFaPowerOff id="icon" className={className} iconsize={iconsize} />,
       anchor: <StyledFaAnchor id="icon" className={className} iconsize={iconsize} />,
       arrowAltCircleRight: <StyledFaArrowAltCircleRight id="icon" className={className} iconsize={iconsize} />,
+      userCircle: <StyledFaRegUserCircle id="icon" className={className} iconsize={iconsize} />,
+      mail: <StyledFaRegEnvelope id="icon" className={className} iconsize={iconsize} />,
+      id: <StyledFaRegIdCard id="icon" className={className} iconsize={iconsize} />,
+      lock: <StyledFaLock id="icon" className={className} iconsize={iconsize} />,
+      warning: <StyledFaExclamationTriangle id="icon" className={className} iconsize={iconsize} />,
     }[iconname] || <div />
   )
 }
 
 Icon.propTypes = {
   className: PropTypes.string,
-  iconname: PropTypes.oneOf(['power', 'anchor', 'arrowAltCircleRight']),
+  iconname: PropTypes.oneOf(['power', 'anchor', 'arrowAltCircleRight', 'userCircle', 'mail', 'id', 'lock', 'warning']),
   iconsize: PropTypes.oneOf(['xsm', 'sm', 'md', 'lg', 'xlg']),
   // type: PropTypes.oneOf(['stroke', 'fill', 'doutone']),
 }
