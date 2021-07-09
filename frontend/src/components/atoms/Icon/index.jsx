8b7a533bb043c09b21fa8676970d4d993b1b0e00
Breno Mazieiro <breno.mazieiro@gmail.com>
2021-07-09 23:00:03 +0100

    will add arrowRight icon

--- frontend/src/components/atoms/Icon/index.jsx
+++ frontend/src/components/atoms/Icon/index.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import {
   FaPowerOff, FaAnchor, FaArrowAltCircleRight, FaRegUserCircle, FaRegEnvelope, FaRegIdCard, FaLock, FaExclamationTriangle,
 } from 'react-icons/fa'
+import { IoIosArrowRoundForward } from 'react-icons/io'
 import styled, { css } from 'styled-components'
 
 const genericDesing = css`
@@ -19,19 +20,21 @@ const StyledFaRegEnvelope = styled(FaRegEnvelope)`${genericDesing}`
 const StyledFaRegIdCard = styled(FaRegIdCard)`${genericDesing}`
 const StyledFaLock = styled(FaLock)`${genericDesing}`
 const StyledFaExclamationTriangle = styled(FaExclamationTriangle)`${genericDesing}`
+const StyledArrowRight = styled(IoIosArrowRoundForward)`${genericDesing}`
 
 const Icon = ({
   iconname, iconsize, className,
 }) => {
   return (
     {
-      power: <StyledFaPowerOff id="icon" className={className} iconsize={iconsize} />,
       anchor: <StyledFaAnchor id="icon" className={className} iconsize={iconsize} />,
       arrowAltCircleRight: <StyledFaArrowAltCircleRight id="icon" className={className} iconsize={iconsize} />,
-      userCircle: <StyledFaRegUserCircle id="icon" className={className} iconsize={iconsize} />,
-      mail: <StyledFaRegEnvelope id="icon" className={className} iconsize={iconsize} />,
+      arrowRight: <StyledArrowRight id="icon" className={className} iconsize={iconsize} />,
       id: <StyledFaRegIdCard id="icon" className={className} iconsize={iconsize} />,
       lock: <StyledFaLock id="icon" className={className} iconsize={iconsize} />,
+      mail: <StyledFaRegEnvelope id="icon" className={className} iconsize={iconsize} />,
+      power: <StyledFaPowerOff id="icon" className={className} iconsize={iconsize} />,
+      userCircle: <StyledFaRegUserCircle id="icon" className={className} iconsize={iconsize} />,
       warning: <StyledFaExclamationTriangle id="icon" className={className} iconsize={iconsize} />,
     }[iconname] || <div />
   )
@@ -39,9 +42,8 @@ const Icon = ({
 
 Icon.propTypes = {
   className: PropTypes.string,
-  iconname: PropTypes.oneOf(['power', 'anchor', 'arrowAltCircleRight', 'userCircle', 'mail', 'id', 'lock', 'warning']),
+  iconname: PropTypes.oneOf(['power', 'anchor', 'arrowAltCircleRight', 'userCircle', 'mail', 'id', 'lock', 'warning', 'arrowRight']),
   iconsize: PropTypes.oneOf(['xsm', 'sm', 'md', 'lg', 'xlg']),
-  // type: PropTypes.oneOf(['stroke', 'fill', 'doutone']),
 }
 
 export default Icon
