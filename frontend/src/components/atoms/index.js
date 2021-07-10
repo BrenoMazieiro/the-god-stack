de008224aaabc0a98923521f0b6256a9617a4599
Breno Mazieiro <breno.mazieiro@gmail.com>
2021-07-10 21:52:35 +0100

    will add checkbox components

--- frontend/src/components/atoms/index.js
+++ frontend/src/components/atoms/index.js
@@ -1,5 +1,6 @@
 export { default as A } from './A'
 export { default as Button } from './Button'
+export { default as Checkbox } from './Checkbox'
 export { default as Div } from './Div'
 export { default as Form } from './Form'
 export { default as Icon } from './Icon'
