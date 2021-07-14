6df63850c99d5c809c3e14f780b2418093e05fa4
Breno Mazieiro <breno.mazieiro@gmail.com>
2021-07-14 19:35:08 +0100

    will make direct import

--- frontend/src/components/molecules/IconButton/index.jsx
+++ frontend/src/components/molecules/IconButton/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
-import { Div, Button, Icon } from 'components'
+import { Div, Button, Icon } from 'components/atoms'
 
 const Wrapper = styled(Div)`
   display: flex;
