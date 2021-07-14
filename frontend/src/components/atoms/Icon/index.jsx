e2bc504ff2162d9ddb557ddf5fe627a87847514c
Breno Mazieiro <breno.mazieiro@gmail.com>
2021-07-14 19:34:47 +0100

    will refactor icon

--- frontend/src/components/atoms/Icon/index.jsx
+++ frontend/src/components/atoms/Icon/index.jsx
@@ -4,38 +4,21 @@ import {
   FaPowerOff, FaAnchor, FaArrowAltCircleRight, FaRegUserCircle, FaRegEnvelope, FaRegIdCard, FaLock, FaExclamationTriangle,
 } from 'react-icons/fa'
 import { IoIosArrowRoundForward } from 'react-icons/io'
-import styled, { css } from 'styled-components'
-
-const genericDesing = css`
-  stroke-width: 1;
-  width: ${({ theme, iconsize }) => theme.sizes.icon[iconsize]};
-  height: ${({ theme, iconsize }) => theme.sizes.icon[iconsize]};
-`
-
-const StyledFaPowerOff = styled(FaPowerOff)`${genericDesing}`
-const StyledFaAnchor = styled(FaAnchor)`${genericDesing}`
-const StyledFaArrowAltCircleRight = styled(FaArrowAltCircleRight)`${genericDesing}`
-const StyledFaRegUserCircle = styled(FaRegUserCircle)`${genericDesing}`
-const StyledFaRegEnvelope = styled(FaRegEnvelope)`${genericDesing}`
-const StyledFaRegIdCard = styled(FaRegIdCard)`${genericDesing}`
-const StyledFaLock = styled(FaLock)`${genericDesing}`
-const StyledFaExclamationTriangle = styled(FaExclamationTriangle)`${genericDesing}`
-const StyledArrowRight = styled(IoIosArrowRoundForward)`${genericDesing}`
 
 const Icon = ({
-  iconname, iconsize, className,
+  iconname, className, color,
 }) => {
   return (
     {
-      anchor: <StyledFaAnchor id="icon" className={className} iconsize={iconsize} />,
-      arrowAltCircleRight: <StyledFaArrowAltCircleRight id="icon" className={className} iconsize={iconsize} />,
-      arrowRight: <StyledArrowRight id="icon" className={className} iconsize={iconsize} />,
-      id: <StyledFaRegIdCard id="icon" className={className} iconsize={iconsize} />,
-      lock: <StyledFaLock id="icon" className={className} iconsize={iconsize} />,
-      mail: <StyledFaRegEnvelope id="icon" className={className} iconsize={iconsize} />,
-      power: <StyledFaPowerOff id="icon" className={className} iconsize={iconsize} />,
-      userCircle: <StyledFaRegUserCircle id="icon" className={className} iconsize={iconsize} />,
-      warning: <StyledFaExclamationTriangle id="icon" className={className} iconsize={iconsize} />,
+      anchor: <FaAnchor id="icon" className={className} color={color} />,
+      arrowAltCircleRight: <FaArrowAltCircleRight id="icon" className={className} color={color} />,
+      arrowRight: <IoIosArrowRoundForward id="icon" className={className} color={color} />,
+      id: <FaRegIdCard id="icon" className={className} color={color} />,
+      lock: <FaLock id="icon" className={className} color={color} />,
+      mail: <FaRegEnvelope id="icon" className={className} color={color} />,
+      power: <FaPowerOff id="icon" className={className} color={color} />,
+      userCircle: <FaRegUserCircle id="icon" className={className} color={color} />,
+      warning: <FaExclamationTriangle id="icon" className={className} color={color} />,
     }[iconname] || <div />
   )
 }
@@ -43,7 +26,7 @@ const Icon = ({
 Icon.propTypes = {
   className: PropTypes.string,
   iconname: PropTypes.oneOf(['power', 'anchor', 'arrowAltCircleRight', 'userCircle', 'mail', 'id', 'lock', 'warning', 'arrowRight']),
-  iconsize: PropTypes.oneOf(['xsm', 'sm', 'md', 'lg', 'xlg']),
+  color: PropTypes.string,
 }
 
 export default Icon
