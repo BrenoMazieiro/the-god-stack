d6355678bf81e95a362fa7f01bb0678ca14740c8
Breno Mazieiro <breno.mazieiro@gmail.com>
2021-07-17 18:22:59 +0100

    will make cypress ci more verbose

--- integration-tests/cypress.sh
+++ integration-tests/cypress.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-yarn install
+echo "Will install dependencies..."
+yarn install --production=false
+echo "Will list directory..."
+ls -lah
+echo "Will show where is..."
+pwd
+echo "Will install cypress..."
 yarn cy:install
+echo "Will run cypress..."
 yarn cy:run
