7f78f8bf358365eaf56f3423ae72915338692bec
Breno Mazieiro <breno.mazieiro@gmail.com>
2021-07-17 17:56:20 +0100

    will change tests and component to new testing-library

--- frontend/src/components/atoms/Button/index.test.js
+++ frontend/src/components/atoms/Button/index.test.js
@@ -1,24 +1,17 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { render, screen, fireEvent } from 'test'
 import Button from '.'
 
-const wrap = ({ type, size, children }) => shallow(<Button type={type} size={size}>{children}</Button>)
-
 describe('Button', () => {
-  it('will render a Button component', () => {
-    const wrapper = wrap({ children: 'Teste', type: 'submit', size: 'lg' })
-    expect(wrapper.contains('Teste')).toBe(true)
+  it('will render a Button', () => {
+    render(<Button id="Ahref" type="button" href="http://test.com">Test</Button>)
+    expect(screen.getByRole('button')).toHaveTextContent('Test')
   })
 
-  it('will render a large Button component when no size is sent', () => {
-    const wrapper = wrap({ children: 'Teste', type: 'submit' })
-    expect(wrapper.find({ size: 'lg' })).toHaveLength(1)
-    expect(wrapper.find({ size: 'sm' })).toHaveLength(0)
-    expect(wrapper.find({ size: 'md' })).toHaveLength(0)
-  })
-
-  it('will render a Button size', () => {
-    const wrapper = wrap({ children: 'Teste', type: 'submit', size: 'sm' })
-    expect(wrapper.find({ size: 'sm' })).toHaveLength(1)
+  it('will test onClick on a Button', () => {
+    const handleClick = jest.fn()
+    render(<Button id="Ahref" type="button" href="http://test.com" onClick={handleClick}>Test</Button>)
+    fireEvent.click(screen.getByText(/Test/i))
+    expect(handleClick).toHaveBeenCalledTimes(1)
   })
 })
